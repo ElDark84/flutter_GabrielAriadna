6bb740ed127a159e04c617144cf470cacf760f16
ariadna <ari.palau20@gmail.com>
2025-04-30 18:35:46 +0200

    primer commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_application_gabriel_ariadna
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_application_gabriel_ariadna/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_application_gabriel_ariadna
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
