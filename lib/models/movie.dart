240a221811e6cbc9cb186a9c7e693492433fa3df
Gabriel <contactodark84@gmail.com>
2025-05-06 19:13:33 +0200

    Attributes Movie

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -7,3 +7,14 @@ class Movie {
   final String releaseDate;
   final String? name; // Para series de TV
   final String? firstAirDate; // Para series de TV
+
+  Movie({
+    required this.id,
+    required this.title,
+    required this.overview,
+    this.posterPath,
+    required this.voteAverage,
+    required this.releaseDate,
+    this.name,
+    this.firstAirDate,
+  });
