523f9503510d512d2fc6586a0c0f25491d7d14dc
Gabriel <contactodark84@gmail.com>
2025-05-06 19:11:53 +0200

    Class Movie

---
+++ lib/models/movie.dart
@@ -0,0 +1,9 @@
+class Movie {
+  final int id;
+  final String title;
+  final String overview;
+  final String? posterPath;
+  final double voteAverage;
+  final String releaseDate;
+  final String? name; // Para series de TV
+  final String? firstAirDate; // Para series de TV
