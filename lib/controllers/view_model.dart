f28762968ad77eed74f90b312e1f8dae05ed3611
Gabriel <contactodark84@gmail.com>
2025-05-06 19:00:40 +0200

    Update view_model.dart

--- lib/controllers/view_model.dart
+++ lib/controllers/view_model.dart
@@ -17,3 +17,11 @@ class MovieViewModel extends ChangeNotifier {
   int _currentPage = 1;
   int _totalPages = 1;
   bool _hasMoreContent = true;
+
+  // Getters
+  List<Movie> get movies => _filteredMovies;
+  List<Review> get reviews => _filteredReviews;
+  bool get isLoading => _isLoading;
+  String get error => _error;
+  ContentType get selectedType => _selectedType;
+  bool get hasMoreContent => _hasMoreContent;
