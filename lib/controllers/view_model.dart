0d5754177b3582e8e7d665b7347255d5d0612c8b
Gabriel <contactodark84@gmail.com>
2025-05-06 18:58:30 +0200

    Class ViewModel

--- lib/controllers/view_model.dart
+++ lib/controllers/view_model.dart
@@ -1,1 +1,19 @@
-//hola 
+import 'package:flutter/foundation.dart';
+import '../models/movie.dart';
+import '../models/review.dart';
+import '../services/movie_service.dart';
+
+enum ContentType { movies, tvShows, reviews }
+
+class MovieViewModel extends ChangeNotifier {
+  final MovieService _movieService = MovieService();
+  List<Movie> _movies = [];
+  List<Movie> _filteredMovies = [];
+  List<Review> _reviews = [];
+  List<Review> _filteredReviews = [];
+  bool _isLoading = false;
+  String _error = '';
+  ContentType _selectedType = ContentType.movies;
+  int _currentPage = 1;
+  int _totalPages = 1;
+  bool _hasMoreContent = true;
