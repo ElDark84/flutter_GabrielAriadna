f9669d1cea509dd28e55f24931e2b98413e624ab
Gabriel <contactodark84@gmail.com>
2025-05-06 19:21:39 +0200

    Class + Attributes Review

---
+++ lib/models/review.dart
@@ -0,0 +1,12 @@
+class Review {
+  final String author;
+  final String content;
+  final double rating;
+  final String createdAt;
+
+  Review({
+    required this.author,
+    required this.content,
+    required this.rating,
+    required this.createdAt,
+  });
